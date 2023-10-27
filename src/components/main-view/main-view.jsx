9885393ed7e599fc876cd7493bf71b996e3c95b0
Matt Gaughan <mattgaugs@gmail.com>
2023-10-27 11:12:53 -0400

    potential fix #2

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,7 +8,7 @@ import "./main-view.scss";
 
 export const MainView = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="mg-portfolio">
       <NavigationBar />
       <Row>
         <Routes>
