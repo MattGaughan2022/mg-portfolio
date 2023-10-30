4c9ff887ddf1e44d64fe8207535ef28c0f52cbae
Matt Gaughan <mattgaugs@gmail.com>
2023-10-30 18:50:17 -0400

    fix home page

--- src/components/home-view/home-view.jsx
+++ src/components/home-view/home-view.jsx
@@ -12,17 +12,17 @@ export const HomeView = () => {
   return (
     <Row className="mx-auto justify-content-md-center">
       <div className="profile">
-        <img
+        <div style={{ height: "200px" }}></div>
+        {/* <img
           src={meJPG}
           className="img__me"
           alt="Actually me.jpg"
           width="130"
           height="200"
           style={{ borderRadius: "10%" }}
-        />
+        /> */}
         <h1>Hello, World!</h1>
-        <p>My name it Matt Gaughan.</p>
-        <p>I am a computer engineer from Boston. </p>
+        <br />
         <Nav.Link as={Link} to="/projects">
           <AwesomeButton type="primary" className="projectBtn">
             Projects
