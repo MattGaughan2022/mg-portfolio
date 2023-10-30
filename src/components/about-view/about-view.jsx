bc27f72bb8792f28d7e634920af3772ff1964d21
Matt Gaughan <mattgaugs@gmail.com>
2023-10-30 17:20:54 -0400

    resize and basic info

--- src/components/about-view/about-view.jsx
+++ src/components/about-view/about-view.jsx
@@ -9,8 +9,18 @@ export const AboutView = () => {
       <img
         src={mejpg}
         alt="me dot jpeg"
-        style={{ aspectRatio: "auto", height: "400px" }}
+        style={{ aspectRatio: "auto", height: "400px", width: "180px" }}
       ></img>
+      <p>
+        I am a computer engineer from Canton MA that has been learning
+        full-stack web development for almost a full year on my own.
+      </p>
+      <br />
+      <p>
+        Recently, I finished working at a small law firm where I handled
+        automation of their client intake via a no-code low-code (Zapier)
+        environment.{" "}
+      </p>
 
       <a href="mailto:mattgaugs@gmail.com">mattgaugs@gmail.com</a>
       <br />
