58dad95a5d1b0ef13aac486c8f43939ff6e4f75e
Matt Gaughan <mattgaugs@gmail.com>
2023-10-30 17:24:11 -0400

    reposition content

--- src/components/about-view/about-view.jsx
+++ src/components/about-view/about-view.jsx
@@ -1,32 +1,34 @@
-import { Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 //img import
 import mejpg from "./me.jpg";
 
 export const AboutView = () => {
   return (
     <Row className="mx-auto justify-content-md-center">
-      <h1>About Me</h1>
-      <img
-        src={mejpg}
-        alt="me dot jpeg"
-        style={{ aspectRatio: "auto", height: "400px", width: "220px" }}
-      ></img>
-      <div style={{ maxWidth: "60%" }}>
-        <p>
-          I am a computer engineer from Canton MA that has been learning
-          full-stack web development for almost a full year on my own.
-        </p>
-        <br />
-        <p>
-          Recently, I finished working at a small law firm where I handled
-          automation of their client intake via a no-code low-code (Zapier)
-          environment.{" "}
-        </p>
-      </div>
+      <Col>
+        <div style={{ maxWidth: "60%" }}>
+          <h1>About Me</h1>
+          <img
+            src={mejpg}
+            alt="me dot jpeg"
+            style={{ aspectRatio: 2 / 1, height: "400px", width: "220px" }}
+          ></img>
+          <p>
+            I am a computer engineer from Canton MA that has been learning
+            full-stack web development for almost a full year on my own.
+          </p>
+          <br />
+          <p>
+            Recently, I finished working at a small law firm where I handled
+            automation of their client intake via a no-code low-code (Zapier)
+            environment.{" "}
+          </p>
+        </div>
 
-      <a href="mailto:mattgaugs@gmail.com">mattgaugs@gmail.com</a>
-      <br />
-      <script src="js/tota11y.min.js"></script>
+        <a href="mailto:mattgaugs@gmail.com">mattgaugs@gmail.com</a>
+        <br />
+        <script src="js/tota11y.min.js"></script>
+      </Col>
     </Row>
   );
 };
